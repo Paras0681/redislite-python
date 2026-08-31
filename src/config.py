57668aa77e0caa8b6155657e4a8e01927e4f6134
Paras0681 <paraskamble8268@gmail.com>
2026-08-31 13:11:03 +0530

    Changes: - Replication functionality where user can create a replica of the master server. - INFO command which shows the role and replica data - REPLCONF PSYNC command - Reads data from the CLI server

--- src/config.py
+++ src/config.py
@@ -15,10 +15,24 @@ from dataclasses import dataclass
 class Config:
     host: str = '0.0.0.0'
     port: int = 6379
+    replicaof_host: str = None
+    replicaof_port: str = None
 
 def parse_args(argv=None) -> Config:
     parser = argparse.ArgumentParser(description="A REdis like server in python from scratch.")
     parser.add_argument("--port", type=int, default=6379, help="Port to listen on (default: 6379)")
     parser.add_argument("--host", type=str, default="0.0.0.0", help="Host/Interface to bind to")
+    parser.add_argument("--replicaof", nargs=2, metavar=("HOST", "PORT"), default=None, help="Replicate from the master at HOST PORT (e.g  --replicaof localhost 6379)")
     args = parser.parse_args(argv)
-    return Config(host=args.host, port=args.port)
+
+    replicaof_host, replicaof_port = None, None
+    if args.replicaof is not None:
+        replicaof_host = args.replicaof[0]
+        replicaof_port = int(args.replicaof[1])
+
+    return Config(
+        host=args.host, 
+        port=args.port,
+        replicaof_host=replicaof_host,
+        replicaof_port=replicaof_port,
+    )
--- src/server.py
+++ src/server.py
@@ -6,6 +6,7 @@ Single-Threaded nonblocking TCP server with a select-based event loop.
 import errno
 import socket
 import select
+import secrets
 
 from . import commands
 from . import resp
@@ -15,7 +16,7 @@ from .connection_state import ConnectionState
 
 
 class RedisServer:
-    def __init__(self, host="0.0.0.0", port=6379):
+    def __init__(self, host="0.0.0.0", port=6379, replicaof_host=None, replicaof_port=None):
         self.host = host
         self.port = port
         self.running = True
@@ -28,6 +29,11 @@ class RedisServer:
         self.storage = Storage()
         self.conn_state = ConnectionState(self.storage, self._queue_write)
 
+        self.replication_state = ReplicationState(
+            replicaof_host,
+            replicaof_port
+        )
+
     def start(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -35,7 +41,7 @@ class RedisServer:
         self.server_socket.bind((self.host, self.port))
         self.server_socket.listen(128)
 
-        print(f"Redis-like server listening on {self.host}:{self.port}")
+        print(f"REdislite server listening on {self.host}:{self.port}")
         self._event_loop()
 
     def stop(self):
@@ -67,7 +73,29 @@ class RedisServer:
         self.readers[client_socket] = resp.RESPReader()
         self.out_buffers[client_socket] = bytearray()
         self.client_addresses[client_socket] = address
-        print(f"Connection from address{address}")
+        print(f"Connected (address): {address}")
+
+    def _handle_info(self, client_socket, args):
+        body = self.replication_state.info_selection()
+        self._queue_write(client_socket, resp.encode_bulk_string(body))
+
+    def _handle_replconf(self, client_socket, args):
+        print(f"REPLCONF received: {args}")
+        self._queue_write(client_socket, resp.encode_simple_string("OK"))
+
+    def _empty_rdb_bytes(self) -> bytes:
+        import base64
+        b64 = "UkVESVMwMDEx+glyZWRpcy12ZXIFNy4yLjD6CnJlZGlzLWJpdHPAQPoFY3RpbWXCbQi8ZfoIdXNlZC1tZW3CsMQQAPoIYW9mLWJhc2XAAP/wbjv+wP9aog=="
+        return base64.b64decode(b64)
+
+    def _handle_psync(self, client_socket, args):
+        print(f"PSYNC received: {args}")
+        replid = self.replication_state.master_replid
+        offset = self.replication_state.master_repl_offset
+        self._queue_write(client_socket, resp.encode_simple_string(f"FULLRESYNC {replid} {offset}"))
+
+        rdb_bytes = self._empty_rdb_bytes()
+        self._queue_write(client_socket, f"${len(rdb_bytes)}\r\n".encode() + rdb_bytes)
 
     def _handle_client_readable(self, client_socket):
         if client_socket not in self.client_sockets:
@@ -102,6 +130,19 @@ class RedisServer:
                 continue
 
             cmd_name = command[0].upper()
+
+            if cmd_name == "INFO":
+                self._handle_info(client_socket, command[1:])
+                continue
+
+            if cmd_name == "REPLCONF":
+                self._handle_replconf(client_socket, command[1:])
+                continue
+
+            if cmd_name == "PSYNC":
+                self._handle_psync(client_socket, command[1:])
+                continue
+
             reply = commands.dispatch(self.storage, command)
             self._queue_write(client_socket, reply)
 
@@ -138,12 +179,37 @@ class RedisServer:
             client_socket.close()
         except socket.error:
             pass
-        print(f"Disconnected {address}")
+        print(f"Disconnected (address): {address}")
+
+
+class ReplicationState:
+    def __init__(self, replicaof_host, replicaof_port):
+        self.master_replid = secrets.token_hex(20)
+        self.master_repl_offset = 0
+
+        self.replicaof_host = replicaof_host
+        self.replicaof_port = replicaof_port
+        self.role = "slave" if replicaof_host is not None else "master"
+
+    def info_selection(self) -> str:
+        "Returns the replication section body as REDIS's info format."
+        lines = [
+            "# Replication",
+            f"role:{self.role}",
+            f"master_replid:{self.master_replid}",
+            f"master_repl_offset:{self.master_repl_offset}",
+        ]
+        return "\r\n".join(lines) + "\r\n"
 
 
 def main():
     config = parse_args()
-    server = RedisServer(host=config.host, port=config.port)
+    server = RedisServer(
+        host=config.host, 
+        port=config.port,
+        replicaof_host=config.replicaof_host,
+        replicaof_port=config.replicaof_port,
+    )
     server.start()
 
 
